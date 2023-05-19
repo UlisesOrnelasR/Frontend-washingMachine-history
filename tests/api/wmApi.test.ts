66b1c28f3291cd1e58d27d7adae42cc25daaed47
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-05-19 11:48:28 -0700

    test: Testing on wmApi

--- tests/api/wmApi.test.ts
+++ tests/api/wmApi.test.ts
@@ -6,4 +6,13 @@ describe("Pruebas en wmApi", () => {
     // console.log(process.env.VITE_API_URL);
     expect(wmApi.defaults.baseURL).toBe(process.env.VITE_API_URL);
   });
+
+  test("Todas las peticiones deben de llevar el token en los headers", async () => {
+    const token = "ABC123";
+    localStorage.setItem("token", token);
+    const response = await wmApi.get("/routeExample"); // Peticion a la API
+    // console.log(response);
+    // console.log(response.config.headers["x-token"]);
+    expect(response.config.headers["x-token"]).toBe(token);
+  });
 });
