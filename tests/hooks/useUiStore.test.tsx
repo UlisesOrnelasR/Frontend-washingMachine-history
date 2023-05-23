eac35840bcd7ac34f488f7272c5981142b22b380
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-05-22 17:52:33 -0700

    test: Tests on useUiStore

--- tests/hooks/useUiStore.test.tsx
+++ tests/hooks/useUiStore.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import { renderHook } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 import { useUiStore } from "../../src/hooks/useUiStore";
 import { uiSlice } from "../../src/store/ui/uiSlice";
 import { UiState } from "../../src/models/ui";
@@ -33,5 +33,41 @@ describe("Pruebas en useUiStore", () => {
       openServiceModal: expect.any(Function),
       closeServiceModal: expect.any(Function),
     });
+  });
+
+  test("openServiceModal debe cambiar isServiceModalOpen a true", () => {
+    const mockStore = getMockStore({
+      isServiceModalOpen: false,
+    });
+    const { result } = renderHook(() => useUiStore(), {
+      wrapper: ({ children }) => (
+        <Provider store={mockStore}>{children}</Provider>
+      ),
+    });
+    // console.log(result.current);
+    const { openServiceModal } = result.current;
+    act(() => {
+      openServiceModal();
+    });
+    // console.log(result.current);
+    expect(result.current.isServiceModalOpen).toBeTruthy();
+  });
+
+  test("closeServiceModal  debe cambiar isServiceModalOpen a false ", () => {
+    const mockStore = getMockStore({
+      isServiceModalOpen: true,
+    });
+
+    const { result } = renderHook(() => useUiStore(), {
+      wrapper: ({ children }) => (
+        <Provider store={mockStore}>{children}</Provider>
+      ),
+    });
+    // console.log(result.current);
+    const { closeServiceModal } = result.current;
+    act(() => {
+      closeServiceModal();
+    });
+    expect(result.current.isServiceModalOpen).toBeFalsy();
   });
 });
